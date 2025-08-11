eb21157a93e457531f4ddfbf3b2055059d038bdf
minseung-gang <strause1@nate.com>
2025-08-11 19:40:57 +0900

    feat: 자기소개 섹션 컴포넌트 추가 - Framer Motion을 이용한 별 장식 애니메이션 구현 - 중앙 자기소개 문구 추가

---
+++ src/components/sections/section01/Hero.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { DECORATE_STARS } from '@/constants/animation';
+
+const Hero = () => {
+  return (
+    <div className='relative flex justify-center pt-72'>
+      {DECORATE_STARS.map((star, idx) => (
+        <motion.img
+          key={idx}
+          className='absolute'
+          animate={star.animation}
+          transition={star.transition}
+          width={100}
+          style={star.position}
+          src={star.src}
+          alt={star.id}
+        />
+      ))}
+      <h1 className='text-center text-[clamp(22px,4vw,85px)] leading-24 font-bold text-white'>
+        안녕하세요
+        <br />
+        프론트엔드 개발자
+        <br />
+        강민승입니다.
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
