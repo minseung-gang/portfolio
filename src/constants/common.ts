fbd1c4d871c2659e5d6499f44f1efa0f586f43b4
minseung-gang <strause1@nate.com>
2025-08-11 20:11:23 +0900

    feat: 이미지 경로를 상수로 분리 및 관리

---
+++ src/constants/common.ts
@@ -0,0 +1,21 @@
+export const IMAGES_ROUTE = {
+  mainCharacter: '/images/person.webp',
+  mainStar: '/images/bigStar.webp',
+  stars: {
+    star01: '/images/star01.png',
+    star02: '/images/star02.png',
+    star03: '/images/star03.png',
+    star04: '/images/star04.png',
+  },
+  profile: '/images/profile.webp',
+  project: {
+    happyPill: '/images/project/happyPill.webp',
+    georim: '/images/project/georim.webp',
+    pullbatte: '/images/project/pullbatte.webp',
+  },
+};
+
+export const SOCIAL_ROUTES = {
+  github: 'https://github.com/minseung-gang',
+  blog: 'https://velog.io/@minseung-gang/posts',
+};
