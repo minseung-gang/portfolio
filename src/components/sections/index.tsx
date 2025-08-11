eeeeb67f59ef5b1176c7dbb17af8fce19af09ec6
minseung-gang <strause1@nate.com>
2025-08-11 19:37:31 +0900

    feat: 섹션 컴포넌트 경로를 모아 export하는 index 파일 추가

---
+++ src/components/sections/index.tsx
@@ -0,0 +1,3 @@
+export { default as Section01 } from '@/components/sections/section01/index';
+export { default as Section02 } from '@/components/sections/section02/index';
+export { default as Section03 } from '@/components/sections/section03/index';
