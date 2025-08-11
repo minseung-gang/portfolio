f2c6491d8c1a35878091dac5b675dfc69c89b474
minseung-gang <strause1@nate.com>
2025-08-11 18:40:00 +0900

    feat: 모달 공통컴포넌트 추가

---
+++ src/components/modal/ui/ModalBackDrop.tsx
@@ -0,0 +1,22 @@
+import { useModalContext } from './Modal';
+
+import { cn } from '@/utils/classNames';
+
+interface ModalBackdropProps {
+  className?: string;
+}
+
+const ModalBackDrop: React.FC<ModalBackdropProps> = ({ className }) => {
+  const { closeModal } = useModalContext();
+  const modalBackdropCls = `fixed inset-0 bg-black opacity-25`;
+  return (
+    <div
+      role='presentation'
+      aria-hidden='true'
+      className={cn(modalBackdropCls, className)}
+      onClick={closeModal}
+    />
+  );
+};
+
+export default ModalBackDrop;
---
+++ src/components/modal/ui/ModalContent.tsx
@@ -0,0 +1,13 @@
+import { cn } from '@/utils/classNames';
+
+interface ModalContentProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const ModalContent: React.FC<ModalContentProps> = ({ className, children }) => {
+  const modalContentCls = `absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white pointer-events-auto rounded-md py-[26px] px-[clamp(15px,2vw,32px)] rounded-md shadow-md`;
+  return <div className={cn(modalContentCls, className)}>{children}</div>;
+};
+
+export default ModalContent;
