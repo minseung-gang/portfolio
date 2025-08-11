54023b730be438664edd37b099e87eec11aba9b2
minseung-gang <strause1@nate.com>
2025-08-11 18:23:42 +0900

    feat: 전역 모달 관리용 GlobalContainer 컴포넌트 추가 - useModalStore로 상태 기반 모달 렌더링 - createPortal을 사용해 document.body에 모달 출력

---
+++ src/components/modal/Global.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom';
+
+import type { ModalItem, ModalPropsMap, ModalType } from '@/types/modal';
+
+import useModalStore from '@/stores/useModalStore';
+import ProjectDetailModal from './ProjectDetailModal';
+
+const MODAL_COMPONENTS = {
+  projectDetail: ProjectDetailModal,
+} as const;
+
+const renderModal = <T extends ModalType>(modal: ModalItem<T>, index: number) => {
+  const Component = MODAL_COMPONENTS[modal.type] as React.FC<ModalPropsMap[T]>;
+  return ReactDOM.createPortal(
+    <Component key={index} {...(modal.props as ModalPropsMap[T])} />,
+    document.body,
+  );
+};
+
+const GlobalContainer = () => {
+  const modals = useModalStore((state) => state.modals);
+
+  return (
+    <>
+      {modals.map((modal: ModalItem, index) => {
+        return renderModal(modal, index);
+      })}
+    </>
+  );
+};
+
+export default GlobalContainer;
