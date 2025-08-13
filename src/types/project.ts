55d83ff90d7a12f22e749509f6f232568bb3f0e0
minseung-gang <strause1@nate.com>
2025-08-13 12:16:54 +0900

    feat: BaseProject 타입 추가

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,3 +2,52 @@ export interface ProjectRoute {
   domain?: string;
   github: string;
 }
+
+export interface BaseProject {
+  title: string;
+  period: string;
+  team?: string;
+  teckStack: string[];
+  description: string;
+  actions: {
+    type: string;
+    icon: () => React.JSX.Element;
+    url?: string;
+  }[];
+  features: {
+    icon: string;
+    title: string;
+    items: string[];
+  };
+  contributions: {
+    icon: string;
+    title: string;
+    contents: {
+      icon: string;
+      section: string;
+      items: string[];
+    }[];
+  };
+  troubleShooting?: {
+    icon: string;
+    title: string;
+    contents: {
+      icon: string;
+      title: string;
+      details: {
+        type: string;
+        content: string;
+      }[];
+    }[];
+  }[];
+  screenShot?: {
+    icon?: string;
+    title?: string;
+    images: Record<string, unknown>;
+  };
+  result?: {
+    icon: string;
+    title: string;
+    items: string[];
+  };
+}
