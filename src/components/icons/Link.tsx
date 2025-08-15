d8dd1fc6dad2eaabb8b146dde0b6d48ca2e4d219
minseung-gang <strause1@nate.com>
2025-08-15 12:54:17 +0900

    refactor: props로 size및 className을 받아 적용하도록 수정

--- src/components/icons/Link.tsx
+++ src/components/icons/Link.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
-const Link = () => {
+interface LinkProps {
+  size?: number;
+  color?: string;
+  className?: string;
+}
+
+const Link = ({ size = 40, className = 'text-[#333333] hover:text-[#72767a]' }: LinkProps) => {
   return (
-    <svg width={40} height={40} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg
+      width={size}
+      height={size}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+    >
       <path
-        className='modal-icon'
-        d='M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212'
-        stroke='#1C274C'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        d='M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
-        className='modal-icon'
-        d='M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373'
-        stroke='#1C274C'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        d='M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
