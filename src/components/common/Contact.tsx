b24168e93fd32ad2ad45d3ac13bed9b51b286348
minseung-gang <strause1@nate.com>
2025-08-15 12:52:16 +0900

    refactor: link태그에 css를 컴포넌트에서 관리하도록 변경

--- src/components/common/Contact.tsx
+++ src/components/common/Contact.tsx
@@ -3,11 +3,13 @@ import Link from 'next/link';
 import React from 'react';
 
 const Contact = () => {
+  const baseButtonStyles =
+    'px-[clamp(10px,2vw,32px)] text-[clamp(11px,1vw,16px)] py-[clamp(10px,2vw,20px)] text-white transition-colors hover:bg-[#121314]';
   return (
-    <section className='fixed bottom-2.5 left-1/2 z-3 -translate-1/2'>
+    <section className='fixed bottom-[clamp(10px,2vw,20px)] left-1/2 z-3 -translate-x-1/2'>
       <div className='grid grid-cols-2 overflow-hidden rounded-md border-1 border-[#494949] bg-[rgba(0,1,8,0.7)] mix-blend-difference'>
         {CONTACT_BUTTONS.map((button, idx) => (
-          <Link key={idx} href={button.herf} className={button.className} target='_blank'>
+          <Link key={idx} href={button.herf} className={baseButtonStyles} target='_blank'>
             {button.label}
           </Link>
         ))}
