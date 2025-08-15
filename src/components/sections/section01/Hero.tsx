401911dd133a68f5af95926d6c1b8a1b986cd2dd
minseung-gang <strause1@nate.com>
2025-08-15 12:57:04 +0900

    style: 이미지 width 반응형으로 수정 및 padding 스타일 수정

--- src/components/sections/section01/Hero.tsx
+++ src/components/sections/section01/Hero.tsx
@@ -4,20 +4,19 @@ import { DECORATE_STARS } from '@/constants/hero';
 
 const Hero = () => {
   return (
-    <div className='relative flex justify-center pt-72'>
+    <div className='relative flex justify-center pt-[30vh]'>
       {DECORATE_STARS.map((star, idx) => (
         <motion.img
           key={idx}
-          className='absolute'
+          className='absolute w-[clamp(80px,10vw,100px)]'
           animate={star.animation}
           transition={star.transition}
-          width={100}
           style={star.position}
           src={star.src}
           alt={star.id}
         />
       ))}
-      <h1 className='text-center text-[clamp(22px,4vw,85px)] leading-24 font-bold text-white'>
+      <h1 className='text-center text-[clamp(25px,5vw,65px)] leading-[clamp(10px,7vw,80px)] font-bold text-white'>
         안녕하세요
         <br />
         프론트엔드 개발자
