0d8f8f822d9b0c8224bc76cff0d1dfd52772b001
minseung-gang <strause1@nate.com>
2025-08-15 12:59:46 +0900

    refactor: CONTACT_BUTTONS 상수 내부에 className 필드 제거

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -4,11 +4,9 @@ export const CONTACT_BUTTONS = [
   {
     label: '깃허브',
     herf: SOCIAL_ROUTES.github,
-    className: 'hover:bg-red bg-red px-8 py-5 text-white transition-colors hover:bg-[#121314]',
   },
   {
     label: '블로그',
     herf: SOCIAL_ROUTES.blog,
-    className: 'px-8 py-5 text-white transition-colors hover:bg-[#121314]',
   },
 ];
